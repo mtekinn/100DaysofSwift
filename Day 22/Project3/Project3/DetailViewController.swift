c8513fd590e1cb5c506137bbd1fd5fd3b6581bfa
Muhammet Tekin <tekinmdev@gmail.com>
2024-05-14 13:30:16 +0300

    final commit

--- Day 22/Project3/Project3/DetailViewController.swift
+++ Day 22/Project3/Project3/DetailViewController.swift
@@ -9,21 +9,45 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    var totalImageCount = 0
+    var selectedImageNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Picture \(selectedImageNumber + 1) of \(totalImageCount)"
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
 
-        // Do any additional setup after loading the view.
+
+        if let selectedImage = selectedImage {
+            imageView.image = UIImage(named: selectedImage)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
     }
-    */
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let imageName = selectedImage ?? "Unknown Image"
+        let vc = UIActivityViewController(activityItems: [image, imageName], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+    
 }
