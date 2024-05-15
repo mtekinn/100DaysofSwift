7ebb09d9f43d6b59d6852c5d2f16bc84577b42d4
Muhammet Tekin <tekinmdev@gmail.com>
2024-05-16 01:50:04 +0300

    initial commit

---
+++ Day 24/Project4/Project4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Mehmet Tekin on 16.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
