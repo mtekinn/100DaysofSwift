cd9ddd36610b99b4af60fec51bedd57ce19b7700
Muhammet Tekin <tekinmdev@gmail.com>
2024-05-30 16:24:20 +0300

    Auto Layout with VFL

---
+++ Day 30/project6b/project6b/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project6b
+//
+//  Created by Mehmet Tekin on 30.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
