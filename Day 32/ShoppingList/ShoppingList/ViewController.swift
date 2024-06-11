bb1c0863af46f346f9debeab97ed82671ce41d28
mtekinn <tekinmdev@gmail.com>
2024-06-11 18:34:20 +0300

    initial commit

---
+++ Day 32/ShoppingList/ShoppingList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by Mehmet Tekin on 11.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
